67d91aa9a13974db6d7607d51b015ceaf33f8cef
Trevor Fehrman <trevorfehrman@gmail.com>
2020-08-23 11:30:54 -0700

    firebase init

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA79EoYgqsaELZIxyWEXZWYAcvqul2gHPo',
+  authDomain: 'wavelength-66c43.firebaseapp.com',
+  databaseURL: 'https://wavelength-66c43.firebaseio.com',
+  projectId: 'wavelength-66c43',
+  storageBucket: 'wavelength-66c43.appspot.com',
+  messagingSenderId: '387037127677',
+  appId: '1:387037127677:web:fc2b7991cf6e3634f86f94',
+  measurementId: 'G-4MLJXS9BJK',
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { FirebaseAppProvider, SuspenseWithPerf, useAuth } from 'reactfire';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { firebaseConfig } from './firebase-config';
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [auth.GoogleAuthProvider.PROVIDER_ID, auth.EmailAuthProvider.PROVIDER_ID],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => true,
+  },
+};
+
+const auth = useAuth;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <SuspenseWithPerf fallback={<p>Loading...</p>}>
+        <App />
+      </SuspenseWithPerf>
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
